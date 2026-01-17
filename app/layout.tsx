1aea0a345522cff35332abba004235457cf80c96
deya19 <deyasaleh2@gmail.com>
2026-01-17 18:58:16 +0300

    implement navigation bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import Header from '@/components/Header';
 // import Header from '@/components/Header';
 
 const geistSans = Geist({
@@ -26,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {/* <Header /> */}
+        <Header />
         {children}
       </body>
     </html>
